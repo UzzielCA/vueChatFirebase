59df8426183f318366c6fcd865fbbce55d620c22
Uzziel <uzzielcervantes@gmail.com>
2018-12-26 17:00:46 -0600

    Se comienza con el login

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="">
+    <h3>Por favor acceda con su cuenta de Goggle para continuar</h3>
+    <button type="button" @click="login">Login with google</button>
+  </div>
+</template>
+
+<script>
+  import firebase from 'firebase'
+
+  export default {
+    methods:{
+      login(){
+        var provider = new firebase.auth.GoogleAuthProvider();
+        provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          console.log(result);
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // ...
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          console.log("errorCode", errorCode);
+          console.log("errorMessage", errorMessage);
+          console.log("email", email);
+          console.log("credential", credential);
+        });
+      }
+    }
+  }
+</script>
